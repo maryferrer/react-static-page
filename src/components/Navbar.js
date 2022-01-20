450beb666c573e188786a8a385acaac8481d787f
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-19 16:56:37 -0800

    create Navbar.js component housing Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+        <img src="./logo512.png" alt="React logo" class="logo"></img>
+        <h3 className="header">ReactFacts</h3>
+        <h4 className="description">React Course - Project 1</h4>
+        </nav>
+    )
+};
+
+export default Navbar;
