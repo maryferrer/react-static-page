b6e3b63c57cc824ebb8291b26c69aefc82907bb4
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-19 16:56:21 -0800

    create Main.js component housing body content

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Main() {
+    return (
+        <main className="main-container">
+            <h1 className="body-title">Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+export default Main
