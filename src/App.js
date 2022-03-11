3718fbc39b977792df7724da41fbde67deac797d
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-03-10 17:25:47 -0800

    use state to initialize display to dark more, setState to change state when dark mode is toggled

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+
+    const [darkMode, setDarkMode] = React.useState(true)
+
+    function toggle() {
+        return (
+            setDarkMode(prevMode => !prevMode)
+        )
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={darkMode} toggleDarkMode={toggle} />
+            <Main darkMode={darkMode} />
         </div>
     )
 }
